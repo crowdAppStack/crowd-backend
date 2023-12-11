5a56140c1a92cd5ca81d5f914c281230989444b2
Benoit Lamonica <35692908+benoitlamonica@users.noreply.github.com>
2023-12-11 20:21:45 +0100

    core: little adjustement to have proper starter build (#5)

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// To test the API - TO REMOVE LATER
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'Hello World!',
+    ], 200);
+});
