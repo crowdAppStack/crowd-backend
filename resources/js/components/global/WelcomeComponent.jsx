2667aa7b68cfc03e24219058c10b9d24374c4c92
Benoit Lamonica <35692908+benoitlamonica@users.noreply.github.com>
2023-12-12 12:28:06 +0100

    core: add react configuration + eslint configuration (#7)
    * core: add react configuration + eslint configuration
    
    * fix tests, forgot yarn build

---
+++ resources/js/components/global/WelcomeComponent.jsx
@@ -0,0 +1,12 @@
+export default function WelcomeComponent() {
+  return (
+    <>
+      <div className="bg-blue-300 p-2 rounded-full">
+        <h1 className="text-white text-center">Welcome to the React App</h1>
+      </div>
+      <div className=" bg-pink-300 rounded-full p-4">
+        Je suis un dev back end et j'adore le design
+      </div>
+    </>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  server: {
+    host: true,
+    hmr: { host: 'localhost', protocol: 'ws' },
+    https: false,
+    strictPort: true,
+    port: 3010,
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      refresh: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@': '/resources/js',
+    },
+  },
 });
