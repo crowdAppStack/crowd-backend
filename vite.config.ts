c4a6af67381d260447f070f7a67ea4a4b237111f
Benoit <benoit.lamonica@gmail.com>
2024-01-26 15:44:26 +0100

    use npm package for dynamic router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
-import dynamicRouter from './dynamic-router'
+import dynamicRouter from 'dynamic-router-plugin-vite'
 
 export default defineConfig({
   server: {
